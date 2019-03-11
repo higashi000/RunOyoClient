3d2e6fa03d3188e22b0d7fd09e77eddb3854c8b5
higashi000 <higashi.ict@gmail.com>
2019-03-11 17:38:45 +0900

    [change] reverse order disp TL

--- Twitter/GetTL.rb
+++ Twitter/GetTL.rb
@@ -1,13 +1,18 @@
 require './GetAPI.rb'
 require './inputText.rb'
 
+
 def GetTL
-  @client.home_timeline.each do |tweet|
+  number = 1
+  @client.home_timeline.reverse_each do |tweet|
+    p number
+    puts("")
     p tweet.user.name
     puts("")
     p tweet.user.screen_name
     puts("")
     p tweet.text
     p "------------------------------"
+    number += 1
   end
 end
