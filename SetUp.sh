d6a2c8179d6d48ae95f10b552bb279d66ffbea14
higashi000 <higashi.ict@gmail.com>
2019-03-11 15:54:04 +0900

    [fix] token

--- SetUp.sh
+++ SetUp.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
+echo -n "Please input your API_KEY >> "
+read _ApiKey
+echo -n "Please input your API_SECRET_KEY >> "
+read _ApiSecretKey
 echo -n "Please input your ACCESS_TOKEN >> "
 read _AccessToken
 echo -n "Please input your ACCESS_TOKEN_SECRET >> "
 read _AccessTokenSecret
-echo -n "Please input your CONSUMER_KEY >> "
-read _ConsumerKey
-echo -n "Please input your CONSUMER_SECRET >> "
-read _ConsumerKeySecret
 
-echo "ACCESS_TOKEN = "$_AccessToken"" >> ./Twitter/TwitterAPI.rb
-echo "ACCESS_TOKEN_SECRET = "$_AccessTokenSecret"" >> ./Twitter/TwitterAPI.rb
-echo "CONSUMER_SECRET = "$_ConsumerKeySecret"" >> ./Twitter/TwitterAPI.rb
-echo "CONSUMER_KEY = "$_ConsumerKey"" >> ./Twitter/TwitterAPI.rb
+echo "CONSUMER_KEY = '$_ApiKey'" > ./Twitter/TwitterAPI.rb
+echo "CONSUMER_SECRET = '$_ApiSecretKey'" >> ./Twitter/TwitterAPI.rb
+echo "ACCESS_TOKEN = '$_AccessToken'" >> ./Twitter/TwitterAPI.rb
+echo "ACCESS_TOKEN_SECRET = '$_AccessTokenSecret'" >> ./Twitter/TwitterAPI.rb
--- Twitter/GetAPI.rb
+++ Twitter/GetAPI.rb
@@ -2,8 +2,8 @@ require 'twitter'
 require './TwitterAPI.rb'
 
 @client = Twitter::REST::Client.new do |config|
-  config.consumer_key        = ACCESS_TOKEN
-  config.consumer_secret     = ACCESS_TOKEN_SECRET
-  config.access_token        = CONSUMER_KEY
-  config.access_token_secret = CONSUMER_SECRET
+  config.consumer_key        = CONSUMER_KEY
+  config.consumer_secret     = CONSUMER_SECRET
+  config.access_token        = ACCESS_TOKEN
+  config.access_token_secret = ACCESS_TOKEN_SECRET
 end
