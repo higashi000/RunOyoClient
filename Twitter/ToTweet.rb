6d1dc6f3292ab8570feeb92cb1ac796e11db3c0e
higashi000 <higashi.ict@gmail.com>
2019-03-13 16:19:02 +0900

    [add] get ARGV

--- Twitter/ToTweet.rb
+++ Twitter/ToTweet.rb
@@ -4,18 +4,12 @@ require './inputText.rb'
 TweetEnding = ["ルン", "ルン！", "オヨ〜", ""]
 
 def toTweet()
-  print("Please input Tweet Text\n >> ")
-
-  tweetText = inputText()
-
   nextEnding = rand(0 .. 4)
 
   case nextEnding
   when 2 then
-    @client.update("#{tweetText}\n#{TweetEnding[nextEnding]}")
-    p "Tweeted `#{tweetText}\n#{TweetEnding[nextEnding]}`"
+    @client.update("#{ARGV[1]}\n#{TweetEnding[nextEnding]}")
   else
-    @client.update("#{tweetText}#{TweetEnding[nextEnding]}")
-    p "Tweeted `#{tweetText}#{TweetEnding[nextEnding]}`"
+    @client.update("#{ARGV[1]}#{TweetEnding[nextEnding]}")
   end
 end
--- Twitter/main.rb
+++ Twitter/main.rb
@@ -3,24 +3,10 @@ require './GetTL.rb'
 require './WordSearch.rb'
 require './inputText.rb'
 
-puts("Thank you for using RunOyoClient!!")
-
-loop do
-
-  print("Choice mode(tweet -> 1, get TL -> 2, serach -> 3, finish RunOyoClient -> q!)\n >> ")
-
-  choiceMode = inputText()
-
-  if choiceMode == "1" then
-    toTweet()
-  elsif choiceMode == "2" then
-    GetTL()
-  elsif choiceMode == "3" then
-    WordSearch()
-  elsif choiceMode == "q!" then
-    puts("Also, use it.")
-    break
-  else
-    puts("Please correct input")
-  end
+if ARGV[0] == "1" then
+  toTweet()
+elsif ARGV[0] == "2" then
+  GetTL()
+elsif ARGV[0] == "3" then
+  WordSearch()
 end
