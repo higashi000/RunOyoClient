5848fbc8a5808acb7019823960938418453d1fa2
higashi000 <higashi.ict@gmail.com>
2019-03-15 22:51:19 +0900

    [add] Download OpenSiv3D

--- SetUp.sh
+++ SetUp.sh
@@ -13,3 +13,16 @@ echo "CONSUMER_KEY = '$_ApiKey'" > ./Twitter/TwitterAPI.rb
 echo "CONSUMER_SECRET = '$_ApiSecretKey'" >> ./Twitter/TwitterAPI.rb
 echo "ACCESS_TOKEN = '$_AccessToken'" >> ./Twitter/TwitterAPI.rb
 echo "ACCESS_TOKEN_SECRET = '$_AccessTokenSecret'" >> ./Twitter/TwitterAPI.rb
+
+git clone https://github.com/Siv3D/OpenSiv3D
+mv OpenSiv3D GUI
+
+cd GUI/OpenSiv3D
+
+git checkout -b linux origin/linux
+
+cd Linux
+
+cp -rf resouces ../../projectConfig
+
+cd ../../
