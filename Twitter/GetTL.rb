9920d30f66c94989748c78042ff0f54381468bc9
higashi000 <higashi.ict@gmail.com>
2019-03-16 14:26:23 +0900

    [add] array in TL information

--- Twitter/GetTL.rb
+++ Twitter/GetTL.rb
@@ -1,15 +1,14 @@
 require './GetAPI.rb'
 require './inputText.rb'
 
-
 def GetTL
-  @client.home_timeline.reverse_each do |tweet|
-    puts("")
-    p tweet.user.name
-    puts("")
-    p tweet.user.screen_name
-    puts("")
-    p tweet.text
-    p "------------------------------"
+  i = 0
+  tlInform = Array.new(25, Array.new(3))
+  @client.home_timeline.each do |tweet|
+    tlInform[i][0] = tweet.user.name
+    tlInform[i][1] = tweet.user.screen_name
+    tlInform[i][2] = tweet.text
+    i += 1
   end
+  return tlInform
 end
