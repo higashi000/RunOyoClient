7e7302ce1866f47dc67ffdc2836e8c70f391c969
higashi000 <higashi.ict@gmail.com>
2019-03-16 15:29:22 +0900

    [add] input TL information

--- Twitter/GetTL.rb
+++ Twitter/GetTL.rb
@@ -2,13 +2,12 @@ require './GetAPI.rb'
 require './inputText.rb'
 
 def GetTL
-  i = 0
-  tlInform = Array.new(25, Array.new(3))
-  @client.home_timeline.each do |tweet|
-    tlInform[i][0] = tweet.user.name
-    tlInform[i][1] = tweet.user.screen_name
-    tlInform[i][2] = tweet.text
-    i += 1
+  File.open("TL.csv", "w") do |file|
+    @client.home_timeline.each do |tweet|
+      file.puts(tweet.user.name)
+      file.puts(tweet.user.screen_name)
+      file.puts(tweet.text)
+    end
   end
-  return tlInform
 end
+GetTL()
