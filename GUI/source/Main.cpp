359bbf19e1a0d6eb507d720a4397075789e6364e
higashi000 <higashi.ict@gmail.com>
2019-03-20 14:06:14 +0900

    [add] disp TL

--- GUI/source/Main.cpp
+++ GUI/source/Main.cpp
@@ -2,17 +2,20 @@
 #include <HamFramework.hpp>
 #include "Button.hpp"
 #include "GetTweetText.hpp"
+#include "LoadTL.hpp"
 
 void Main() {
 
   Window::Resize(1280, 780);
   Graphics::SetBackground(Color(0, 169, 104));
 
-  RunOyoClient::GetTweetText textbox;
-  textbox.setPos(100, 100);
+  RunOyoClient::LoadTL tl;
+
+  tl.setPos(10, 10);
+    tl.loadTLCSV();
 
 
   while (System::Update()) {
-    textbox.draw();
+    tl.draw();
   }
 }
