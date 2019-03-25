c1e47ac554f6ae21d4f29e4fa94e7d0af21418ee
higashi000 <higashi.ict@gmail.com>
2019-03-25 22:00:19 +0900

    [add] new Main.cpp

--- GUI/source/Main.cpp
+++ GUI/source/Main.cpp
@@ -9,16 +9,20 @@ void Main() {
   Window::Resize(1280, 780);
   Graphics::SetBackground(Color(0, 169, 104));
 
-  RunOyoClient::LoadTL tl;
 
-  tl.setPos(10, 10);
-  RunOyoClient::Button button;
-  button.setPos(0, 0);
+  RunOyoClient::GetTweetText tweetTextBox;
+  RunOyoClient::LoadTL tl;
+  RunOyoClient::Button updateTL;
+  updateTL = RunOyoClient::Button(140, 50, 40, U"TL更新");
+  tweetTextBox.setPos(0, 0);
+  updateTL.setPos(10, 100);
+  tl.setPos(10, 165);
 
 
   while (System::Update()) {
-    button.draw();
-    if (button.isClick()) {
+    tweetTextBox.draw();
+    updateTL.draw();
+    if (updateTL.isClick()) {
       tl.getTL();
       tl.loadTLCSV();
     }
